5aa63c00192323c0f86e13c2aa0904379adf1dc9
Cam <cam.schnackel@gmail.com>
2017-09-13 17:55:04 -0500

    added sql table entries to database.sql

--- server/database.sql
+++ server/database.sql
@@ -8,3 +8,10 @@ CREATE TABLE koalaHolla (
     ready_for_transfer BOOLEAN,
     notes VARCHAR(200)
 );
+
+INSERT INTO koalaHolla (name, gender, age, ready_for_transfer, notes) VALUES ('Scotty', 'M', 4, true, 'Born in Guatemala');
+INSERT INTO koalaHolla (name, gender, age, ready_for_transfer, notes) VALUES ('Jean', 'F', 5, true, 'Allergic to lots of lava');
+INSERT INTO koalaHolla (name, gender, age, ready_for_transfer, notes) VALUES ('Ororo', 'F', 7, false, 'loves listening to Paula (Abdul)');
+INSERT INTO koalaHolla (name, gender, age, ready_for_transfer, notes) VALUES ('Logan', 'M', 15, false, 'Loves the sauna');
+INSERT INTO koalaHolla (name, gender, age, ready_for_transfer, notes) VALUES ('Charlie', 'M', 9, true, 'Favorite band is Nirvana');
+INSERT INTO koalaHolla (name, gender, age, ready_for_transfer, notes) VALUES ('Betsy', 'F', 4, true, 'Has a pet iguana');
